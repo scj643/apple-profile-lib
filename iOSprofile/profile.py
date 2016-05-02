074b2d4e7baf66f3484d4f04d026525730af716b
Charles Surett <surettcharles@gmail.com>
2016-05-02 08:10:25 -0400

    Revert "Create dicts atomically"

--- iOSprofile/profile.py
+++ iOSprofile/profile.py
@@ -6,25 +6,27 @@ from io import BytesIO
 
 try:
     import Crypto
+
     Crypto_support = True
 except ImportError:
     Crypto_support = False
     print 'No crypto support'
-
 try:
     import biplist
+
     binary_support = True
 except ImportError:
     binary_support = False
     print 'No binary support'
-
 try:
     import PIL
-    from PIL import Image
+
     imgsupport = True
 except ImportError:
     imgsupport = False
     print('No PIL support')
+if imgsupport:
+    from PIL import Image
 
 
 # Defining Exceptions
@@ -40,7 +42,7 @@ class ParamInvalid(Exception):
         self.etype = etype
 
     def __str__(self):
-        return 'Argument {!r} is wrong type, should be {!r}.'.format(self.atrib, self.etype)
+        return 'Argument {!r} is wrong type should be {!r}.'.format(self.atrib, self.etype)
 
 
 def typehandle(value, argn, opt=True, rtype=str):
@@ -94,58 +96,60 @@ class Payloads(object):
         self.profile = []
 
     def font(self, font, ident=uid(), name=None, **kwargs):
+        ident = 'font.' + ident
+        returns = {'PayloadType': 'com.apple.font'}
         if not font:
             return
-        ident = 'font.' + ident
-        returns = {'PayloadType': 'com.apple.font',
-                   'Font': plistlib.Data(font),
-                   'Name': typehandle(name, 'name')}
+        returns['Font'] = plistlib.Data(font)
+        returns['Name'] = typehandle(name, 'name')
         returns = self.common(returns, ident, kwargs)
-        self.profile += [strip(returns)]
+        striped = strip(returns)
+        self.profile += [striped]
 
     def webclip(self, url, label, fullscreen=None, ident=uid(), icon=None,
                 precomposed=True, removable=True, **kwargs):
         ident = 'webclip.' + ident
-        returns = {'PayloadType': 'com.apple.webClip.managed',
-                   'URL': url,
-                   'Label': label,
-                   'IsRemovable': removable,
-                   'Precomposed': typehandle(precomposed, 'precomposed', rtype=bool),
-                   'FullScreen': typehandle(fullscreen, 'fullscreen', rtype=bool)}
+        returns = {'PayloadType': 'com.apple.webClip.managed', 'URL': url,
+                   'Label': label, 'IsRemovable': removable}
         if icon and imgsupport:
             img = Image.open(icon) if isinstance(icon, str) else icon
             data_buffer = BytesIO()
             img.save(data_buffer, 'PNG')
             icon_data = data_buffer.getvalue()
             returns['Icon'] = plistlib.Data(icon_data)
+        returns['Precomposed'] = typehandle(precomposed, 'precomposed', rtype=bool)
+        returns['FullScreen'] = typehandle(fullscreen, 'fullscreen', rtype=bool)
         returns = self.common(returns, ident, kwargs)
-        self.profile += [strip(returns)]
+        striped = strip(returns)
+        self.profile += [striped]
 
     def vpn(self, vpntype, alltraffic=False):
         return
 
     def certificate(self, certtype, cert, filename=None, password=None, ident=uid(), **kwargs):
+        returns = {}
         if not cert or not certtype in ('root', 'pkcs1', 'pem', 'pkcs12'):
             return
-        returns = {'PayloadType': 'com.apple.security.' + certtype,
-                   'PayloadContent': plistlib.Data(cert),
-                   'PayloadCertificateFilename': typehandle(filename, 'filename'),
-                   'Password': typehandle(password, 'password')}
+        returns['PayloadType'] = 'com.apple.security.' + certtype
+        returns['PayloadContent'] = plistlib.Data(cert)
+        returns['PayloadCertificateFilename'] = typehandle(filename, 'filename')
+        returns['Password'] = typehandle(password, 'password')
         returns = self.common(returns, ident, kwargs)
         self.profile += [strip(returns)]
 
     def wifi(self, ssid, hidden=False, encryption='Any', hotspot=False, autojoin=True,
              pw=None, ident=uid(), **kwargs):
         ident = 'wifi.' + ident
-        returns = {'PayloadType': 'com.apple.wifi.managed',
-                   'SSID_STR': typehandle(ssid, 'ssid', rtype=bool),
-                   'HIDDEN_NETWORK': typehandle(hidden, 'hidden', rtype=bool),
-                   'AutoJoin': typehandle(autojoin, 'autojoim', rtype=bool),
-                   'Password': typehandle(pw, 'password')}
+        returns = {'PayloadType': 'com.apple.wifi.managed'}
+        returns['SSID_STR'] = typehandle(ssid, 'ssid', rtype=bool)
+        returns['HIDDEN_NETWORK'] = typehandle(hidden, 'hidden', rtype=bool)
+        returns['AutoJoin'] = typehandle(autojoin, 'autojoim', rtype=bool)
         if encryption in ('WEP', 'WPA', 'WPA2', 'Any', 'None'):
             returns['EncryptionType'] = encryption
+        returns['Password'] = typehandle(pw, 'password')
         returns = self.common(returns, ident, kwargs)
-        self.profile += [strip(returns)]
+        striped = strip(returns)
+        self.profile += [striped]
 
     def common(self, content, ident, horg=None, hname=None, hdesc=None, ver=1):
         content['PayloadIdentifier'] = self.config.ident + '.' + ident
@@ -172,14 +176,14 @@ def mkplist(pload):
     :return: Dict representation of plist
     """
     p = strippayload(pload)
-    returns = {'PayloadType': 'Configuration',
-               'PayloadVersion': 1,
+    returns = {'PayloadType': 'Configuration', 'PayloadVersion': 1,
                'PayloadIdentifier': pload.config.ident,
-               'PayloadUUID': uid(),
-               'PayloadDescription': typehandle(pload.config.hdesc, 'hdesc'),
-               'PayloadDisplayName': typehandle(pload.config.hname, 'hdesc'),
-               'PayloadOrganization': typehandle(pload.config.horg, 'horg'),
-               'PayloadContent': pload.profile}
+               'PayloadUUID': uid()}
+
+    returns['PayloadDescription'] = typehandle(pload.config.hdesc, 'hdesc')
+    returns['PayloadDisplayName'] = typehandle(pload.config.hname, 'hdesc')
+    returns['PayloadOrganization'] = typehandle(pload.config.horg, 'horg')
     if pload.config.rdate:
         returns['RemovalDate'] = pload.config.rdate
+    returns['PayloadContent'] = pload.profile
     return returns
